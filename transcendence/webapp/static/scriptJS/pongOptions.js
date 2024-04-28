4ee476dae8e8c5e6360f6a8b285949b529114f09
TastyBao <erard.bastien@gmail.com>
2024-04-28 18:15:06 +0200

    Modal for tournament ; done

--- transcendence/webapp/static/scriptJS/pongOptions.js
+++ transcendence/webapp/static/scriptJS/pongOptions.js
@@ -70,4 +70,55 @@ function	launchPongTournament()
 		updateModalMessage('pong_tournament_modal');
 		return;
 	}
+	selectPlayersNames(selectedPlayers);
 }
+
+// Select the players names for the Pong Tournament
+
+async function	selectPlayersNames(selectedPlayers)
+{
+	let	form = document.getElementById("playerNamesForm");
+	let	fragment = document.createDocumentFragment();
+	let	player = await updateModalTournament();
+	form.innerHTML = '';
+
+	for (let i = 1; i < selectedPlayers; i++)
+	{
+		let	inputGroup = document.createElement("div");
+		inputGroup.classList.add("input-group", "mb-3");
+
+		let inputLabel = document.createElement("label");
+		inputLabel.textContent = player + ` ${i + 1} : `;
+		inputLabel.setAttribute("for", `playerName${i}`);
+		inputLabel.classList.add("form-label", "custom-input");
+
+		let inputField = document.createElement("input");
+		inputField.type = "text";
+		inputField.id = `playerName${i}`;
+		inputField.name = `playerName${i}`;
+		inputField.classList.add("form-control", "custom-input");
+
+		inputGroup.appendChild(inputLabel);
+		inputGroup.appendChild(inputField);
+		fragment.appendChild(inputGroup);
+	}
+	form.appendChild(fragment);
+	let playerNamesModal = new bootstrap.Modal(document.getElementById('tournamentModal'), {
+		backdrop: 'static'
+	});
+	playerNamesModal.show();
+}
+
+// function submitPlayerNames() {
+//     let form = document.getElementById("playerNamesForm");
+//     let names = [];
+//     for (let element of form.elements) {
+//         if (element.type === "text") {
+//             names.push(element.value);
+//         }
+//     }
+//     console.log(names); // Affiche ou traite les noms ici
+//     // Ferme le modal
+//     let playerNamesModal = bootstrap.Modal.getInstance(document.getElementById('tournamentModal'));
+//     playerNamesModal.hide();
+// }
--- transcendence/webapp/static/scriptJS/translation.js
+++ transcendence/webapp/static/scriptJS/translation.js
@@ -1,13 +1,19 @@
+// This file is used to handle the translation of the website
+
 async function	fetchLanguageData(lang)
 {
 	const response = await fetch(`/static/languages/${lang}.json`);
 	return response.json();
 }
 
+// Set the language preference in the local storage
+
 function	setLanguagePreference(lang)
 {
 	localStorage.setItem('language', lang);
 }
+
+// Update the content of the website with the new language data
 
 function	updateContent(langData)
 {
@@ -17,12 +23,16 @@ function	updateContent(langData)
 	});
 }
 
+// Change the language of the website
+
 async function	changeLanguage(lang)
 {
 	await setLanguagePreference(lang);
 	const langData = await fetchLanguageData(lang);
 	updateContent(langData);
 }
+
+// Event listener to change the language of the website when DOM is loaded
 
 document.addEventListener('DOMContentLoaded', async () => {
 	const userPreferredLanguage = localStorage.getItem('language') || 'en';
@@ -31,6 +41,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 	document.querySelector('.form-select.custom-select').value = userPreferredLanguage;
 });
 
+// Update the Error modal message with the correct language
+
 async function	updateModalMessage(errorKey)
 {
 	const lang = localStorage.getItem('language') || 'en';
@@ -38,4 +50,14 @@ async function	updateModalMessage(errorKey)
 	document.getElementById('errorMessage').textContent = langData[errorKey];
 	let alertModal = new bootstrap.Modal(document.getElementById('errorModal'));
 	alertModal.show();
+}
+
+// Update the Tournament modal message with the correct language
+
+async function	updateModalTournament()
+{
+	const	lang = localStorage.getItem('language') || 'en';
+	const	langData = await fetchLanguageData(lang);
+	const	player = langData['player'];
+	return (player);
 }
