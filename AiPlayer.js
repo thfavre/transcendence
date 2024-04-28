ee06a22d05af61b85f54e4023469f2b5de4fe7aa
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-04-28 16:10:06 +0200

    working version of AI will now be testing raycasting

--- AiPlayer.js
+++ AiPlayer.js
@@ -50,6 +50,10 @@ export default class AiPlayer extends Player {
 		console.log("wallArray: ", this.wallArray);
 	}
 
+
+	// ---------- Preparing the walls and goal ----------
+
+
 	// create the wall object based on the startPos, and endPos
 	createWall(startPos, endPos)
 	{
@@ -58,8 +62,26 @@ export default class AiPlayer extends Player {
 			endPos: new THREE.Vector3(endPos.x, endPos.y, endPos.z),
 			slope: this.calculateSlope(startPos, endPos),
 			angle: this.getAngle(),
-			normal: this.calculateNormal(startPos, endPos)
+			normal: this.calculateNormal(startPos, endPos),
+			equation: (x) => this.calculateSlope(startPos, endPos) * x + (startPos.y - this.calculateSlope(startPos, endPos) * startPos.x)
 		};
+	}
+
+	// returns the angle between walls based on nb of players
+	getAngle()
+	{
+		switch (this.totalPlayers) {
+			case 2:
+				return 90;
+			case 3:
+				return 60;
+			case 4:
+				return 90;
+			case 5:
+				return 72;
+			default:
+				return null;
+		}
 	}
 
 	calculateNormal(startPos, endPos)
@@ -83,6 +105,10 @@ export default class AiPlayer extends Player {
 	{
 		return ((endPos.y - startPos.y) / (endPos.x - startPos.x));
 	}
+
+
+
+	// ---------- Predicting ball position ----------
 
 	// calculates the velocity after it reflects on a wall
 	calculateReflectedVelocity(ballVelocity, wallNormal) {
@@ -115,70 +141,115 @@ export default class AiPlayer extends Player {
 	}
 
 
-	// true if ball is behind the wall
-	isBehindGoal(ballPosition, slope)
-	{
-		const b = -(slope * ballPosition.x) + ballPosition.y;
-		if (b < 0)
-		{
-			console.log("TRUE FOR BEHIND THE WALL");
-			return true;
+	// // true if ball is behind the wall
+	// isBehindGoal(ballPosition, slope)
+	// {
+	// 	const b = -(slope * ballPosition.x) + ballPosition.y;
+	// 	if (b < 0)
+	// 	{
+	// 		console.log("TRUE FOR BEHIND THE WALL");
+	// 		return true;
+	// 	}
+	// 	else
+	// 		return false;
+	// }
+
+	// pointsDistance(startPos, endPos)
+	// {
+	// 	return Math.sqrt(Math.pow((endPos.x - startPos.x), 2) + Math.pow((endPos.y - startPos.y), 2));
+	// }
+
+	// // return true if the ball is behind the wall
+	// isBehindWall(startPos, endPos, ballPos)
+	// {
+	// 	const threshold = 5;
+	// 	const wallLen = this.pointsDistance(startPos, endPos) + threshold;
+	// 	const startToBall = this.pointsDistance(startPos, ballPos);
+	// 	const endToBall = this.pointsDistance(endPos, ballPos);
+
+	// 	const sumDistances = startToBall + endToBall;
+
+	// 	// console.log("sumDistances: ", sumDistances);
+	// 	// console.log("wallLen: ", wallLen);
+
+	// 	if ((sumDistances < wallLen) || (sumDistances > wallLen))
+	// 		return false;
+	// 	else
+	// 	{
+	// 		return true;
+	// 	}
+	// }
+
+	wallCollision(ballPosition, ballVelocity, wallStartPos, wallEndPos, ballRadius) {
+		// Vector from start to end of the wall
+		let wallVector = new THREE.Vector3().subVectors(wallEndPos, wallStartPos);
+
+		// Vector from ball's position to start of the wall
+		let ballToWallStart = new THREE.Vector3().subVectors(wallStartPos, ballPosition);
+
+		// Project ball-to-wall vector onto wall vector
+		let projection = ballToWallStart.dot(wallVector.normalize());
+
+		// Closest point on wall to ball's position
+		let closestPointOnWall = wallStartPos.clone().add(wallVector.clone().multiplyScalar(projection));
+
+		// Calculate distance between ball's position and closest point on wall
+		let distanceToWall = ballPosition.distanceTo(closestPointOnWall);
+
+		// Check if the distance is less than or equal to the sum of the ball's radius and a small threshold (to handle floating point errors)
+		if (distanceToWall <= ballRadius + 0.01) {
+			// Collision detected, calculate reflection of velocity vector
+			let wallNormal = wallVector.clone().normalize(); // Normal vector of the wall
+			let reflection = ballVelocity.clone().reflect(wallNormal); // Reflect ball's velocity off the wall
+			return { collision: true, reflection: reflection };
+		} else {
+			// No collision
+			return { collision: false, reflection: null };
 		}
-		else
-			return false;
 	}
 
-	pointsDistance(startPos, endPos)
+
+	// returns true if the pointsToCheck(x, y) is on the wall (wallStartPos, wallEndPos)
+	pointOnSegment(pointToCheck, wallStartPos, wallEndPos, wallSlope)
 	{
-		return Math.sqrt(Math.pow((endPos.x - startPos.x), 2) + Math.pow((endPos.y - startPos.y), 2));
+		// const expectedY = wallSlope * (pointToCheck.x - wallStartPos.x) + wallStartPos.y;
+
+		// // if (pointToCheck.y !== expectedY)
+		// // 	return false;
+
+		// if (Math.abs(pointToCheck.y - expectedY) > Number.EPSILON)
+		// return false;
+
+		// if ((pointToCheck.x >= Math.min(wallStartPos.x, wallEndPos.x)) && (pointToCheck.x <= Math.max(wallStartPos.x, wallEndPos.x)))
+		// 	return true;
+
+		// return false;
 	}
 
-	// return true if the ball is behind the wall
-	isBehindWall(startPos, endPos, ballPos)
+	// pointOnSegment(wall, pointToCheck)
+	// {
+	// 	let x = ((wall.endPos.y - wall.slope * wall.endPos.x) - (wall.startPos.y - wall.slope * wall.startPos.x)) /
+	// }
+
+	calculateYIntercept(startPos, slope)
 	{
-		const threshold = 5;
-		const wallLen = this.pointsDistance(startPos, endPos) + threshold;
-		const startToBall = this.pointsDistance(startPos, ballPos);
-		const endToBall = this.pointsDistance(endPos, ballPos);
-
-		const sumDistances = startToBall + endToBall;
-
-		// console.log("sumDistances: ", sumDistances);
-		// console.log("wallLen: ", wallLen);
-
-		if ((sumDistances < wallLen) || (sumDistances > wallLen))
-			return false;
-		else
-		{
-			return true;
-		}
+		return (startPos.y - slope * startPos.x);
 	}
 
-	wallCollision( ballPos, ballVelocity, wallStart, wallEnd, ballRadius)
+	calculateBallSlope(ball)
 	{
-		const wallVec = new THREE.Vector2().copy(wallEnd).sub(wallStart);
-
-		const ballToWall = new THREE.Vector2().copy(wallStart).sub(ballPos);
-
-		const distance = Math.abs(ballToWall.dot(new THREE.Vector2(wallVec.y, wallVec.x))) / wallVec.length();
-
-		// console.log("wall normal: ", wallVec);
-		// console.log("ball Radius: ", ballRadius);
-		// console.log()
-		// console.log("distance: ", distance, " vs ballRadius " , ballRadius);
-
-		if (distance <= 3 * ballRadius) {
-			// Calculate the dot product between the ball's velocity and the wall's normal vector
-			const dotProduct = ballVelocity.dot(new THREE.Vector2(wallVec.y, wallVec.x));
-			console.log("dot product: ", dotProduct);
-
-			// If the dot product is negative, the ball is moving towards the wall
-			if (dotProduct < 0) {
-				return true; // Collision detected
-			}
-		}
-		return false; // No collision detected
+		return ball.body.velocity.y / ball.body.velocity.x;
 	}
+
+	// return the intersection point of the ball and the wall
+	getInter(ball, wall)
+	{
+		let x = (this.calculateYIntercept(ball.body.position, this.calculateBallSlope(ball)) - (this.calculateYIntercept(wall.startPos, wall.slope))) / (this.calculateBallSlope(ball) - wall.slope);
+
+		let y = wall.equation(x);
+		return new THREE.Vector2(x, y);
+	}
+
 
 	// predicts the position of the ball after time in ms
 	getBallPositionTime(ballPosition, ballVelocity, time) {
@@ -194,88 +265,98 @@ export default class AiPlayer extends Player {
 		return predictedPosition;
 	}
 
-	pointOnSegment(pointToCheck, wallStartPos, wallEndPos, wallSlope) {
-		const expectedY = wallSlope * (pointToCheck.x - wallStartPos.x) + wallStartPos.y;
-
-		// if (pointToCheck.y !== expectedY)
-		// 	return false;
-
-		if (Math.abs(pointToCheck.y - expectedY) > Number.EPSILON)
-			return false;
-
-		if ((pointToCheck.x >= Math.min(wallStartPos.x, wallEndPos.x)) && (pointToCheck.x <= Math.max(wallStartPos.x, wallEndPos.x)))
-			return true;
-
-		return false;
-	}
-
 
 	// get the new position where the ball intersects with the goal
 	predictBallPosition(ballPosition, ballVelocity)
 	{
-		const simTime = 2000;
+		const simTime = 1000;
 		const incrTime = 50;
-		let predictedBallPos;
 
 
-		// predictedBallPos = getBallPositionTime(ballPosition, ballVelocity, currTime);
-		for (let t = 0; t < simTime; t += incrTime)
-		{
-			predictedBallPos = this.getBallPositionTime(ballPosition, ballVelocity, t);
-			if (this.pointOnSegment(predictedBallPos, this.goal.startPos, this.goal.endPos, this.goal.slope))
-				break;
-			for (var i = 0; i < this.wallArray.length; i++)
-			{
-				if (this.pointOnSegment(predictedBallPos, this.wallArray[i].startPos, this.wallArray[i].endPos, this.wallArray[i].slope))
-				{
-					console.log("ball velocity before reflection: ", ballVelocity);
-					ballVelocity = this.calculateFinalVelocity(ballVelocity, this.wallArray[i].normal);
-					console.log("ball velocity after reflection: ", ballVelocity);
-					break ;
+		let predictedBallPos = ballPosition.clone(); // Initialize predicted position with current ball position
+		let currVelocity = ballVelocity.clone(); // Initialize current velocity
+
+		for (let t = 0; t < simTime; t += incrTime) {
+			// Check for collision with walls
+			for (let i = 0; i < this.wallArray.length; i++) {
+				const collisionInfo = this.wallCollision(predictedBallPos, currVelocity, this.wallArray[i].startPos, this.wallArray[i].endPos, this.ball.radius);
+				if (collisionInfo.collision) {
+					// Collision detected, update velocity
+					currVelocity = collisionInfo.reflection;
+					// Update predicted position with new velocity
+					predictedBallPos = this.getBallPositionTime(predictedBallPos, currVelocity, incrTime);
+					break;
 				}
 			}
 
-			// if (this.wallCollision(predictedBallPos, ballVelocity, this.goal.startPos, this.goal.endPos, this.ball.radius))
-			// 	break;
-			// for (var i = 0; i < this.wallArray.length; i++)
-			// {
-			// 	if (this.wallCollision(predictedBallPos, ballVelocity, this.wallArray[i].startPos, this.wallArray[i].endPos, this.ball.radius))
-			// 	{
-			// 		console.log("ball velocity before reflection: ", ballVelocity);
-			// 		ballVelocity = this.calculateFinalVelocity(ballVelocity, this.wallArray[i].normal);
-			// 		console.log("ball velocity after reflection: ", ballVelocity);
-			// 		break ;
-			// 	}
-			// }
+			// Update predicted position with current velocity
+			predictedBallPos = this.getBallPositionTime(predictedBallPos, currVelocity, incrTime);
+
+			// Check if ball reaches goal
+			if (this.pointOnSegment(predictedBallPos, this.goal.startPos, this.goal.endPos, this.goal.slope)) {
+				break;
+			}
 		}
+
+		// predictedBallPos = getBallPositionTime(ballPosition, ballVelocity, currTime);
+		// for (let t = 0; t < simTime; t += incrTime)
+		// {
+		// 	predictedBallPos = this.getBallPositionTime(ballPosition, ballVelocity, t);
+
+		// 	for (let i = 0; i < this.wallArray.length; i++) {
+		// 		if (this.wallCollision(predictedBallPos, ballVelocity, this.wallArray[i].startPos, this.wallArray[i].endPos, this.ball.radius)) {
+		// 			// Collision detected, update velocity
+		// 			ballVelocity = this.calculateFinalVelocity(ballVelocity, this.wallArray[i].normal);
+		// 			// Update predicted position with new velocity
+		// 			predictedBallPos = this.getBallPositionTime(predictedBallPos, ballVelocity, incrTime);
+		// 			break;
+		// 		}
+		// 	}
+
+		// 	// Check if ball reaches goal
+		// 	if (this.pointOnSegment(predictedBallPos, this.goal.startPos, this.goal.endPos, this.goal.slope)) {
+		// 		break;
+		// 	}
+		// 	// if (this.pointOnSegment(predictedBallPos, this.goal.startPos, this.goal.endPos, this.goal.slope))
+		// 	// 	break;
+		// 	// for (var i = 0; i < this.wallArray.length; i++)
+		// 	// {
+		// 	// 	if (this.pointOnSegment(predictedBallPos, this.wallArray[i].startPos, this.wallArray[i].endPos, this.wallArray[i].slope))
+		// 	// 	{
+		// 	// 		console.log("ball velocity before reflection: ", ballVelocity);
+		// 	// 		ballVelocity = this.calculateFinalVelocity(ballVelocity, this.wallArray[i].normal);
+		// 	// 		console.log("ball velocity after reflection: ", ballVelocity);
+		// 	// 		break ;
+		// 	// 	}
+		// 	// }
+
+		// 	// if (this.wallCollision(predictedBallPos, ballVelocity, this.goal.startPos, this.goal.endPos, this.ball.radius))
+		// 	// 	break;
+		// 	// for (var i = 0; i < this.wallArray.length; i++)
+		// 	// {
+		// 	// 	if (this.wallCollision(predictedBallPos, ballVelocity, this.wallArray[i].startPos, this.wallArray[i].endPos, this.ball.radius))
+		// 	// 	{
+		// 	// 		console.log("ball velocity before reflection: ", ballVelocity);
+		// 	// 		ballVelocity = this.calculateFinalVelocity(ballVelocity, this.wallArray[i].normal);
+		// 	// 		console.log("ball velocity after reflection: ", ballVelocity);
+		// 	// 		break ;
+		// 	// 	}
+		// 	// }
+		// }
 		// console.log("predictedBallPos: ", predictedBallPos);
 		// this.drawSphere(predictedBallPos);
 
 		return predictedBallPos;
 	}
 
-	// returns the angle between walls based on nb of players
-	getAngle()
-	{
-		switch (this.totalPlayers) {
-			case 2:
-				return 90;
-			case 3:
-				return 60;
-			case 4:
-				return 90;
-			case 5:
-				return 72;
-			default:
-				return null;
-		}
-	}
+	// ---------- Working DO NOT TOUCH ----------
 
 	updateBall(ball)
 	{
 		this.ball = ball;
 		const	ballVelocity = new THREE.Vector3(ball.body.velocity.x, ball.body.velocity.y, 0);
 		this.targetPosition = this.predictBallPosition(ball.body.position, ballVelocity);
+		console.log("targetPosition: ", this.targetPosition);
 		// this.drawSphere(this.targetPosition);
 
 	}
@@ -285,14 +366,16 @@ export default class AiPlayer extends Player {
 			return; // Do nothing if there's no target position
 		}
 		const	startToTarget = this.startPos.distanceTo(this.targetPosition);
-		const	startToPaddle = this.startPos.distanceTo(this.paddlePosition);
+		const	startToPaddle = this.startPos.distanceTo(this.paddle.body.position);
 		const	endToTarget = this.endPos.distanceTo(this.targetPosition);
-		const	endToPaddle = this.endPos.distanceTo(this.paddlePosition);
+		const	endToPaddle = this.endPos.distanceTo(this.paddle.body.position);
 
 		if (startToTarget < startToPaddle)
 			this.paddle.moveDown();
 		else if (endToTarget < endToPaddle)
 			this.paddle.moveUp();
+
+			// console.log("targetPosition: ", this.targetPosition, " paddlePosition:")
 	}
 
 	drawSphere(newPosition) {
