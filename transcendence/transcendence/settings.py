1fe0806de19e9238e82cf4be4af3a52b3e29b63d
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-05-06 17:22:32 +0200

    adding .env applying it to secrets in config

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z=jl4c)y)1yhoivdpa!^u5r&x___m8jvxh&)4z6-5=xx!wb*@x'
+SECRET_KEY = env('SECRET_KEY')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True  # Redirect all non-HTTPS requests to HTTPS
@@ -83,9 +87,9 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # Matches the database name in docker-compose
-        'USER': 'postgres',     # Matches the username in docker-compose
-        'PASSWORD': 'postgres', # Matches the password in docker-compose
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'db',           # Hostname to reach your db container from web container
         'PORT': '5432',         # Default PostgreSQL port
     }
