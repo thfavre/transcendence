54751ccc5442d174c2fe0b458148a376ef649c81
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-05-06 13:15:55 +0200

    fixed quick model gameResult issue

--- transcendence/webapp/static/scriptJS/history.js
+++ transcendence/webapp/static/scriptJS/history.js
@@ -75,64 +75,66 @@ async function updateTable(tableContainerId, game_id) {
 
 // NO TOUCH
 
-function sendGameData(gameHistory) {
-    // Make sure gameData is a JavaScript object containing the necessary data
+function sendGameData(gameHistory)
+{
+	// Make sure gameData is a JavaScript object containing the necessary data
 
-    // Convert gameData to JSON
+	// Convert gameData to JSON
 	const username = localStorage.getItem('userAlias')
 	const csrfToken = getCookie('csrftoken')
-    const jsonData = JSON.stringify({username: username, gameHistory: gameHistory});
+	const jsonData = JSON.stringify({username: username, gameHistory: gameHistory});
 
-    // Send JSON data to the backend using fetch API
-    fetch('/save_game_result/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken // Include CSRF token in headers
-        },
-        body: jsonData
-    })
-    .then(response => {
-        if (response.ok) {
-            // Game data saved successfully
-            console.log('Game history saved successfully');
-        } else {
-            // Error handling if saving failed
-            console.error('Failed to save game data');
-        }
-    })
-    .catch(error => {
-        console.error('Error sending game data:', error);
-    });
+	// Send JSON data to the backend using fetch API
+	fetch('/save_game_result/', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': csrfToken // Include CSRF token in headers
+		},
+		body: jsonData
+	})
+	.then(response => {
+		if (response.ok) {
+			// Game data saved successfully
+			console.log('Game history saved successfully');
+		} else {
+			// Error handling if saving failed
+			console.error('Failed to save game data');
+		}
+	})
+	.catch(error => {
+		console.error('Error sending game data:', error);
+	});
 }
 
 // retrieves the last 10 games of 'game_id' associated to 'username'
-function getGameHistory(username, game_id) {
-    return new Promise((resolve, reject) => {
-        fetch(`/get_game_history?username=${username}&game_id=${game_id}`, {
-            method: 'GET'
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Failed to fetch game history data.');
-            }
-        })
-        .then(data => {
-            if (data.error) {
-                console.error("Error:", data.error);
-                reject(data.error); // Reject the Promise if there's an error
-            } else {
-                console.log("Game history:", data);
-                resolve(data); // Resolve the Promise with the game history data
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            reject(error); // Reject the Promise if there's an error
-        });
-    });
+function getGameHistory(username, game_id)
+{
+	return new Promise((resolve, reject) => {
+		fetch(`/get_game_history?username=${username}&game_id=${game_id}`, {
+			method: 'GET'
+		})
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error('Failed to fetch game history data.');
+			}
+		})
+		.then(data => {
+			if (data.error) {
+				console.error("Error:", data.error);
+				reject(data.error); // Reject the Promise if there's an error
+			} else {
+				console.log("Game history:", data);
+				resolve(data); // Resolve the Promise with the game history data
+			}
+		})
+		.catch(error => {
+			console.error(error);
+			reject(error); // Reject the Promise if there's an error
+		});
+	});
 }
 
 // retrieves the last game played by the user
