3d1423783ac1338cd7aca129aa3544ba0ea29e16
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-05-06 17:11:09 +0200

    fuck it...

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -134,15 +134,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = "/app/staticfiles"
-# print('STATIC_ROOT:', os.path.abspath(STATIC_ROOT))
 
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "webapp/templates"),
-# ]
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
