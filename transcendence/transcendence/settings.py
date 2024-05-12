be561dfeaaed9516073a83adf4e3cd91846d37e9
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-05-12 14:50:33 +0200

    Update database configuration

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'db',           # Hostname to reach your db container from web container
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),           # Hostname to reach your db container from web container
         'PORT': '5432',         # Default PostgreSQL port
     }
 }
